d4fa265ce60d6644f21df033b056902d677ce1d9
Orange Foxie <vinhphuc931998@gmail.com>
2021-08-03 23:43:31 +0700

    code

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function sum(a){
     let arr1 = new Array()
     let kq = 0;
@@ -6,19 +5,27 @@ function sum(a){
         a = a/2
         arr1.push(a)        
     }
-    kq = Math.floor(arr1.reduce((a,b)=>{return a+b}))
-    return kq;
+    try {
+        kq = Math.floor(arr1.reduce((a,b)=>{
+            if(a && b){return a+b}
+            else{ return 0}
+        }))
+        return {kq,arr1};
+      } catch (error) {
+        console.log(`Đang chờ giá trị: '${error.message}'`);
+      }
 }
 
 function Cal(slKem){  
-    if(!isNaN(slKem)){
-        document.getElementById("ketQua").innerHTML = sum(slKem)
+    if(slKem && !isNaN(slKem) && slKem>1){
+        document.getElementById("ketQua").innerHTML = (sum(slKem).kq)
+        document.getElementById("tinhNham").innerHTML = (sum(slKem).arr1).join(" | ")
     }else{
-        document.getElementById("ketQua").innerHTML = "Đang chờ"
+        document.getElementById("ketQua").innerHTML = "where's my kem"
     }
 }
 
 function main(){    
     let slKem = document.getElementById("slKem").value
-    slKem.onkeyup = Cal(slKem)
-}main()
+    Cal(slKem)
+}
